17d77d8ee95b9fdfac376cdf20cf85cf8a586f47
sqwww <2569362147@qq.com>
2019-11-05 12:32:53 +0800

    上传登录界面

--- main.js
+++ main.js
@@ -11,42 +11,51 @@ function newRandom (maxnum){
 
 // 卡牌
 class Card {
-	// this.chosen = false;
 	constructor(name,suit,point) {
-		this.name = name;
-		this.suit = suit;
-		this.point = point
-	}
-	// 创建一个button标签
-	creatjQuery(){
-		return $("<button class=\'" + this.name +"\'>" + this.name +" "+ this.suit + this.point + "</button>")
+			this.owner=null;
+			this.object=null;
+			this.name=name;
+			this.suit=suit;
+			this.point=point;
+			// "fixed","notchosen","chosen"
+			this.chosen="fixed";
+			this.effect= ()=>{
+				switch (this.name){
+					case "sha":
+						return function () {
+							
+						}
+						break;
+					case"shan":
+						break;
+					case"tao":
+						break;
+					case"guo":
+						break;
+					default:
+						break;
+				}
+			}
+			this.cardButton=$("<button></button>")
+					.addClass("this.name")
+					.attr("type","button")
+					.css("background-color",this.chosen==="fixed"?"gray":"white")
+					.text(this.name+" "+this.suit+this.point)
 	}
 }
 
 // 手牌
 class Hand extends Array{
-	// Uncaught Reference Error:
-	// Must call super constructor in derived class before accessing 'this' or returning from derived constructor
-	constructor(player) {
+	constructor(person) {
 		super();
-		this.owner = player
+		this.owner = person
 	}
-	remove(card){
-		if(card === 'random'){
-			this.splice(newRandom(this.length-1),1)
-		}else{
-			this.splice(this.indexOf(element),1);
-			}
-	}
-	add(card){
-		this.push(card)
-	}
-	show(){
+
+	// 把button添加到html中
+	showCards(){
 		this.map(function(current){
-			$("#" + this + "Hand").append(current.creatjQuery());
-			// console.log("#" + owner + "Hand")
-			// console.log($("#" + owner + "Hand"))
-		},this.owner)
+			$("#" + this + "Hand").append(current.cardButton)
+		},this.owner.body.player)
 	}
 }
 
@@ -71,9 +80,7 @@ class Library extends Array{
 
 // 玩家
 class Person {
-	constructor(player,nation,sex,name,hp,skill) {		
-		this.skill = skill;
-		this.hand = new Hand(player);
+	constructor(player,nation,sex,name,hp,skill) {
 		this.body = {
 			player:player,
 			nation:nation,
@@ -82,35 +89,38 @@ class Person {
 			maxHp:hp,
 			hp:hp,
 		};
-	}
-	show(){
-		
+		this.skill=skill
+		this.hand = new Hand(this);
 	}
 	draw(num,library){
 		for(let i = 0;i < num;i++){
-			this.hand.add(library[0])
-			library.shift()
+			library[0].owner = this;
+			this.hand.push(library[0]).showCards();
+			library.shift();
 		}
+	}	
+	remove(card){
+		if(card === 'random'){
+			card = this.hand(newRandom(this.hand.length-1))
+		}
+		card.owner = card.object = card.cardButton = null;
+		this.hand.splice(this.hand.indexOf(card),1).showCards()
 	}
-	useCard (card,object){
+	useCard(card){
 		switch (card.name){
 			case "sha":
-				object.body.hp--;
+				if
 				break;
-			case "shan":
-				this.body.hp++;
+			case"shan":
 				break;
-			case "tao":
-				this.body.hp++;
+			case"tao":
 				break;
-			case "guo":
-				object.hand.remove("random");
+			case"guo":
 				break;
 			default:
 				break;
 		}
-		this.hand.remove(card);
-	}	
+	}
 }	
 
 
@@ -120,47 +130,40 @@ function playStart(){
 	var player1 = new Person("player1","Qun","male","LvBu",4,null);
 	var player2 = new Person("player2","Qun","female","Diaochan",3,null);
 	var library = new Library(50);
-	
 	[player1,player2].forEach(function(current){
-		current.draw(4,library);
-		current.hand.show();
+		current.hand.draw(4,library);
+		current.hand.showCards();
 	})
 	
 	
 	// // 使用闭包把回合封装起来
-	// function round() {
+	function round() {
 		
 		
-	// 	// 判别回合的主宾
-	// 	if(order%2 == 1){
-	// 		subject = player1
-	// 		object = player2
-	// 	}else{
-	// 		subject = player2;
-	// 		object = player1
-	// 	}
+		// 判别回合的主宾
+		order%2 === 1?player1.object = player2:player2.object = player1
 		
 	// 	// 检查血量
 	// 	if(subject.body.hp < 1){
 	// 		(prompt("游戏结束！是否要开始新的一局？") === "")?gameRestart():gameEnd();
 	// 	}
 		
-	// 	// 摸牌
-	// 	if(order === 1){
-	// 		subject.draw(1,library)
-	// 	}else{
-	// 		subject.draw(2,library)
-	// 	}
+		// 摸牌
+		if(order === 1){
+			subject.hand.draw(1,library)
+		}else{
+			subject.hand.draw(2,library)
+		}
 		
 	// 	// 打牌
 		
 		
 	// 	//弃牌
-	// 	if(subject.hand.length > subject.body.hp){
-	// 		for(let i = 0;i < (subject.hand.length - subject.body.hp);i++){
-	// 			subject.hand.remove("random")
-	// 		}
-	// 	}
+		if(subject.hand.length > subject.body.hp){
+			for(let i = 0;i < (subject.hand.length - subject.body.hp);i++){
+				subject.hand.remove("random")
+			}
+		}
 		
 	// 	// 准备下一回合
 	// 	order++;
@@ -182,11 +185,11 @@ function gameStart () {
 
 // 重新游戏
 function gameRestart(){
-	// 清除当前所有数据
+	// 清除当前所有数据***
+	
 	
 	$("#mainpage").hide();
-	$("#start").show();
-		
+	$("#start").show();	
 }
 //结束对局
 function gameEnd () {
